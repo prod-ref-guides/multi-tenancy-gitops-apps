13a4a749665dcdf5376b036a88ef5225cbb5aeb2
Orhan-Yarar <orhan.yarar@ibm.com>
2022-01-11 10:22:19 -0500

    GitOps customizations for organization and cluster
    Signed-off-by: Orhan-Yarar <orhan.yarar@ibm.com>

--- ace/environments/ci/secrets/ibm-entitled-key-secret.sh
+++ ace/environments/ci/secrets/ibm-entitled-key-secret.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 # Set variables
-IBM_ENTITLEMENT_KEY=<ENTITLEMENT-KEY>
-SEALEDSECRET_NAMESPACE=sealed-secrets
+#IBM_ENTITLEMENT_KEY=<ENTITLEMENT-KEY>
+IBM_ENTITLEMENT_KEY=${IBM_ENTITLEMENT_KEY}
+#SEALEDSECRET_NAMESPACE=sealed-secrets
+SEALED_SECRET_NAMESPACE=${SEALED_SECRET_NAMESPACE:-sealed-secrets}
+SEALED_SECRET_CONTOLLER_NAME=${SEALED_SECRET_CONTOLLER_NAME:-sealed-secrets}
 
 # Create Kubernetes Secret yaml
 oc create secret docker-registry ibm-entitlement-key \
@@ -12,7 +15,8 @@ oc create secret docker-registry ibm-entitlement-key \
 --dry-run=true -o yaml > delete-ibm-entitled-key-secret.yaml
 
 # Encrypt the secret using kubeseal and private key from the cluster
-kubeseal --scope cluster-wide --controller-name=sealedsecretcontroller-sealed-secrets --controller-namespace=${SEALEDSECRET_NAMESPACE} -o yaml < delete-ibm-entitled-key-secret.yaml > ibm-entitled-key-secret.yaml
+#kubeseal --scope cluster-wide --controller-name=sealedsecretcontroller-sealed-secrets --controller-namespace=${SEALEDSECRET_NAMESPACE} -o yaml < delete-ibm-entitled-key-secret.yaml > ibm-entitled-key-secret.yaml
+kubeseal -n ci --controller-name=${SEALED_SECRET_CONTOLLER_NAME} --controller-namespace=${SEALED_SECRET_NAMESPACE} -o yaml < delete-ibm-entitled-key-secret.yaml > ibm-entitled-key-secret.yaml
 
 # NOTE, do not check delete-ibm-entitled-key-secret.yaml into git!
-rm delete-ibm-entitled-key-secret.yaml
+ rm delete-ibm-entitled-key-secret.yaml
